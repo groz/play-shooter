6c5304d5d2b934293276eede0ff0745d9a11154e
Hasan <hasan.akniyev@gmail.com>
2015-10-05 17:07:14 +0300

    Added Vector2 method stubs

--- app/models/Vector2.scala
+++ app/models/Vector2.scala
@@ -1,3 +1,15 @@
 package models
 
-case class Vector2(x: Double, y: Double)
+case class Vector2(x: Double, y: Double) {
+  def scale(x : Double) : Vector2 = ???
+
+  def rotate(angle: Double) : Vector2 = ???
+
+  def add(other: Vector2) : Vector2 = ???
+
+  def length : Double = ???
+
+  def angleTo (other: Vector2) : Double = ???
+  
+  def * (other: Vector2) : Double = ???
+}

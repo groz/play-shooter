cdac5c979959fcbd2e87c1d017519df535e67a1b
temeer <sharapudinov@gmail.com>
2015-10-05 18:08:43 +0300

    Added rotate and rotatecw

--- app/models/Vector2.scala
+++ app/models/Vector2.scala
@@ -3,7 +3,9 @@ package models
 case class Vector2(x: Double, y: Double) {
   def scale(mult : Double) : Vector2 = Vector2(x * mult, y * mult)
 
-  def rotate(angle: Double) : Vector2 = ???
+  def rotate(angle: Double) : Vector2 = Vector2 (this.x*math.cos(angle) - y* math.sin(angle), x*math.sin(angle) + y*math.cos(angle))
+
+  def rotateCw(angle: Double) : Vector2 = rotate(-angle) 
 
   def add(other: Vector2) : Vector2 = Vector2(x + other.x, y + other.y)
 

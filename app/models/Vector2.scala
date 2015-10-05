eac1ff82ed982ddf8d6781fba374e0a74978c619
Hasan <hasan.akniyev@gmail.com>
2015-10-05 17:14:05 +0300

    Added some Vector2 methods

--- app/models/Vector2.scala
+++ app/models/Vector2.scala
@@ -1,15 +1,15 @@
 package models
 
 case class Vector2(x: Double, y: Double) {
-  def scale(x : Double) : Vector2 = ???
+  def scale(mult : Double) : Vector2 = Vector2(x * mult, y * mult)
 
   def rotate(angle: Double) : Vector2 = ???
 
-  def add(other: Vector2) : Vector2 = ???
+  def add(other: Vector2) : Vector2 = Vector2(x + other.x, y + other.y)
 
-  def length : Double = ???
+  def length : Double = math.sqrt(x * x + y * y)
 
   def angleTo (other: Vector2) : Double = ???
-  
-  def * (other: Vector2) : Double = ???
+
+  def * (other: Vector2) : Double = x * other.x + y * other.y
 }
